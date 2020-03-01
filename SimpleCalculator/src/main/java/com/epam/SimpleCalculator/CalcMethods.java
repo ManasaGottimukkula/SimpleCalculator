a8dc80a900a5d337c38ae9e8797652a523affc5a
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-01 18:26:46 +0530

    added operations in CalcMethods Class

--- SimpleCalculator/src/main/java/com/epam/SimpleCalculator/CalcMethods.java
+++ SimpleCalculator/src/main/java/com/epam/SimpleCalculator/CalcMethods.java
@@ -2,6 +2,20 @@ package com.epam.SimpleCalculator;
 
 public class CalcMethods {
 	
-
+	public double addition(double a, double b) {
+		return a+b;
+	}
+	public double subtract(double a, double b) {
+		return a-b;
+	}
+	public double multiply(double a, double b) {
+		return a*b;
+	}
+	public double division(double a, double b) {
+		return a/b;
+	}
+	public double modulus(double a, double b) {
+		return a%b;
+	}
 
 }
