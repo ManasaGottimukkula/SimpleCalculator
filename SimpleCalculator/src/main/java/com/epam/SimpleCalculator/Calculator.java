0b7804720ce139fc5d13247c0cfe225bec5961eb
ManasaGottimukkula <17wh1a05b6@bvrithyderabad.edu.in>
2020-03-01 19:54:48 +0530

    Updated Calculator class

--- SimpleCalculator/src/main/java/com/epam/SimpleCalculator/Calculator.java
+++ SimpleCalculator/src/main/java/com/epam/SimpleCalculator/Calculator.java
@@ -1,9 +1,94 @@
 package com.epam.SimpleCalculator;
+import java.util.*;
+
+import java.util.Scanner;
+
+
 
 public class Calculator {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println("enter the first value: ");
+		double x = sc.nextDouble();
+		System.out.println("enter the second value: ");
+		double y = sc.nextDouble();
+		
+		CalcMethods method = new CalcMethods();
+		double result;
+		boolean choose = true;
+		char another;
+		while(choose) {
+			
+		System.out.println("Select operations from below\n1. Addition(+)\n2. Substraction(-)\n3. Multiplication(*)\n4. Division(/)\n5. Modulus(%)");
+		try {
+			switch(sc.next().charAt(0)) {
+			case '1':
+				result = method.addition(x, y);
+				System.out.println(x+" + "+y+ "= "+result+"\nDo you want to perform any other operation(y/n)?");
+				another = sc.next().charAt(0);
+				if(another == 'y'|| another == 'Y') {
+					choose = true;
+					break;
+				}
+				else {
+					choose = false;
+					
+				}
+			case '2':
+				result = method.subtract(x, y);
+				System.out.println(x+" - "+y+ "= "+result+"\nDo you want to perform any other operation(y/n)?");
+				another = sc.next().charAt(0);
+				if(another == 'y'|| another == 'Y') {
+					choose = true;
+					break;
+				}
+				else {
+					choose = false;
+					
+				
+			}
+			case '3':
+				result = method.multiply(x, y);
+				System.out.println(x+" * "+y+ "= "+result+"\nDo you want to perform any other operation(y/n)?");
+				another = sc.next().charAt(0);
+				if(another == 'y'|| another == 'Y') {
+					choose = true;
+					break;
+				}
+				else {
+					choose = false;
+					
+				}
+			case '4':
+				result = method.division(x, y);
+				System.out.println(x+" / "+y+ "= "+result+"\nDo you want to perform any other operation(y/n)?");
+				another = sc.next().charAt(0);
+				if(another == 'y'|| another == 'Y') {
+					choose = true;
+					break;
+				}
+				else {
+					choose = false;
+					
+				}
+			case '5':
+				result = method.modulus(x, y);
+				System.out.println(x+" % "+y+ "= "+result+"\nDo you want to perform any other operation(y/n)?");
+				another = sc.next().charAt(0);
+				if(another == 'y'|| another == 'Y') {
+					choose = true;
+					break;
+				}
+				else {
+					choose = false;
+				}
+				default :
+					System.out.println("Please select from the operations mentioned above..");
+			}
+		}catch(Exception e) {
+			System.out.println("Invalid selection!!!!!");
+		}
 	}
-
+}
 }
